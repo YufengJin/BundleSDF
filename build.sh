5e72b1566500dd437493be971b3478af05f11532
YufengJin <jyf931118@gmail.com>
2024-03-19 15:12:39 +0100

    tested environments for both bundlesdf and gaussian splattingw

--- build.sh
+++ build.sh
@@ -2,3 +2,6 @@ ROOT=$(pwd)
 cd /kaolin && pip install -e .
 cd ${ROOT}/mycuda && rm -rf build *egg* && pip install -e .
 cd ${ROOT}/BundleTrack && rm -rf build && mkdir build && cd build && cmake .. && make -j11
+cd ${ROOT}/gaussian-splatting && pip install submodules/diff-gaussian-rasterization
+cd ${ROOT}/gaussian-splatting && pip install submodules/simple-knn                      
+~                                                                                                  
