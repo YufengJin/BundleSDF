6724df8e4dc4d86e95f90519deadc986a1c1b2c4
YufengJin <jyf931118@gmail.com>
2024-03-19 20:29:51 +0100

    fixed Syntax error

--- build.sh
+++ build.sh
@@ -4,4 +4,3 @@ cd ${ROOT}/mycuda && rm -rf build *egg* && pip install -e .
 cd ${ROOT}/BundleTrack && rm -rf build && mkdir build && cd build && cmake .. && make -j11
 cd ${ROOT}/gaussian-splatting && pip install submodules/diff-gaussian-rasterization
 cd ${ROOT}/gaussian-splatting && pip install submodules/simple-knn                      
-~                                                                                                  
