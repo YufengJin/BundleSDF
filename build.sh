0700c241cff18c6efbccb1768a112f4ccc4102e7
YufengJin <jyf931118@gmail.com>
2024-04-19 07:20:19 +0200

    merged environments bundlesdf+gsSLAM

--- build.sh
+++ build.sh
@@ -2,5 +2,6 @@ ROOT=$(pwd)
 cd /kaolin && pip install -e .
 cd ${ROOT}/mycuda && rm -rf build *egg* && pip install -e .
 cd ${ROOT}/BundleTrack && rm -rf build && mkdir build && cd build && cmake .. && make -j11
-cd ${ROOT}/gaussian-splatting && pip install -e submodules/diff-gaussian-rasterization
-cd ${ROOT}/gaussian-splatting && pip install -e submodules/simple-knn                      
+cd ${ROOT}/submodules && pip install -e submodules/diff-gaussian-rasterization-w-depth
+cd ${ROOT}/submodules && pip install -e submodules/simple-knn                      
+cd ${ROOT} && pip install -r requirements.txt
